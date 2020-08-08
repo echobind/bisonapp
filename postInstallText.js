ef50954434999bfb0ba2a7397a0a0bd083c1abbd
Chris Ball <chris@echobind.com>
2020-08-08 09:37:47 -0400

    updates

--- postInstallText.js
+++ postInstallText.js
@@ -7,6 +7,7 @@ cd %NAME%
 * Run \`vc\` to link your project to vercel.
 * Migrate your database with \`yarn db:migrate\`. You will be prompted to create it if necessary.
 * Run the app with \`yarn dev\`. This will start the dev server and watchers for generating types.
-* Create a new GitHub repo, and push the code from your newly generated repo.
-* Enjoy a full CI setup including E2E tests!
+* Create a new GitHub repo, and push the code from your newly generated app.
+
+See the https://github.com/echobind/bisonapp#recommended-dev-workflow for tips and further setup.
 `;
