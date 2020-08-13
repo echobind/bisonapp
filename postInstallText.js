08bfad492d623ece8cf345246483acd9c930e948
Chris Ball <chris@echobind.com>
2020-08-13 13:00:57 -0400

    feat(db): adds db:setup script (#18)

--- postInstallText.js
+++ postInstallText.js
@@ -4,7 +4,7 @@ All set! To finalize setup, do the following:
 
 cd %NAME%
 
-* Migrate your database with \`yarn db:migrate\`. You will be prompted to create it if necessary.
+* Setup your database with \`yarn db:setup\`. You will be prompted to create it if necessary.
 * Run the app with \`yarn dev\`. This will start the dev server and watchers for generating types.
 * Create a new GitHub repo, and push the code from your newly generated app.
 * Set ENV vars for CI and Deployment
