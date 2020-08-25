02e4a93c5047c118f1afe0906ed41803491a5c38
Chris Ball <chris@echobind.com>
2020-08-25 13:18:22 -0400

    feat(deployment): support Heroku (#38)
    * add PORT to start script
    
    * upgrade cypress to allow deploy
    
    * move universal-cookie to devDependencies
    
    * write cacheDirectories in package.json for heroku
    
    * add inquirer, prompt for host
    
    * add to ci
    
    * deploy from GH actions
    
    * fix graphql handler
    
    * add heroku specific items to CI
    
    * updates to support heroku
    
    * heroku : dont create apps if user says no
    
    * put back commented out things
    
    * interpolate prisma env

--- cli.js
+++ cli.js
@@ -1,5 +1,78 @@
 #!/usr/bin/env node
 const createBisonApp = require(".");
+const inquirer = require("inquirer");
+const Logo = require("./logo");
+const execa = require("execa");
+
+/**
+ * Generates questions for Inquirer based on an app name.
+ * @param {string} appName The name of the app
+ */
+function generateQuestions(appName) {
+  return [
+    {
+      name: "githubRepo",
+      type: "input",
+      message: "Create a new GitHub repo and paste the url here:",
+    },
+    {
+      name: "host.name",
+      type: "list",
+      message: "Where will you deploy the app?",
+      choices: [
+        { name: "Vercel (recommended)", value: "vercel" },
+        { name: "Heroku", value: "heroku" },
+      ],
+      default: "vercel",
+    },
+    {
+      name: "host.createAppsAndPipelines",
+      type: "confirm",
+      message: "Do you want to automatically create apps and pipelines?",
+      when: (answers) => answers.host.name === "heroku",
+      default: true,
+    },
+    {
+      name: "host.staging.name",
+      type: "input",
+      message: "Enter the name for the staging app (must be unique)",
+      when: ({ host }) => host.name === "heroku" && host.createAppsAndPipelines,
+      default: `${appName}-staging`,
+    },
+    {
+      name: "host.staging.db",
+      type: "list",
+      message: "What database tier do you want on staging?",
+      choices: ["heroku-postgresql:hobby-dev", "heroku-postgresql:standard-0"],
+      when: ({ host }) => host.name === "heroku" && host.createAppsAndPipelines,
+      default: "heroku-postgresql:hobby-dev",
+    },
+    {
+      name: "host.production.name",
+      type: "input",
+      message: "Enter the name for the production app (must be unique)",
+      when: ({ host }) => host.name === "heroku" && host.createAppsAndPipelines,
+      default: `${appName}`,
+    },
+    {
+      name: "host.production.db",
+      type: "list",
+      message: "What database tier do you want on production?",
+      choices: ["heroku-postgresql:hobby-dev", "heroku-postgresql:standard-0"],
+      when: ({ host }) => host.name === "heroku" && host.createAppsAndPipelines,
+      default: "heroku-postgresql:standard-0",
+    },
+  ];
+}
+
+/**
+ * Verifies a user is logged into the Heroku CLI.
+ * If they aren't
+ */
+async function verifyHerokuLogin() {
+  const { stdout } = await execa("heroku", ["whoami"]);
+  console.log(`Logged into Heroku as ${stdout}`);
+}
 
 require("yargs").usage(
   "$0 <name>",
@@ -10,7 +83,25 @@ require("yargs").usage(
       type: "string",
     });
   },
-  function (yargs) {
-    createBisonApp(yargs.name);
+  async function (yargs) {
+    // Show the logo!
+    console.log(Logo);
+
+    const { name } = yargs;
+    const questions = generateQuestions(name);
+    const answers = await inquirer.prompt(questions);
+    const hostName = answers.host.name;
+
+    if (hostName !== "heroku") createBisonApp({ name, ...answers });
+
+    // If heroku, make sure they are logged in before continuing.
+    try {
+      await verifyHerokuLogin();
+      createBisonApp({ name, ...answers });
+    } catch {
+      console.error(
+        `\n\nIt looks like you're not logged in to Heroku CLI. Use \`heroku login\` and try again.`
+      );
+    }
   }
 ).argv;
---
+++ docs/faq.md
@@ -0,0 +1,9 @@
+# FAQ
+
+## Where are the generated types?
+
+TypeScript Types for GraphQL types, queries, and mutations are generated automatically and placed in `./types.ts`.
+
+## VSCode can't find new types, even though they are in ./types.ts
+
+Try reopening VSCode.
