685dcd8e2a9972eac715fafce0b96abe06fe9691
Alvin Crespo <alvin.crespo@gmail.com>
2020-10-02 11:39:52 -0400

    move to TS and make some code style adjustments

--- template/scripts/dropDatabase.ts
+++ template/scripts/dropDatabase.ts
@@ -1,20 +1,12 @@
 export {};
 
 const url = require('url');
-
 const Client = require('pg').Client;
 
 const connectionString = process.env.DATABASE_URL;
-
-const dbUrl = url.parse(connectionString);
-
-const db = new Client({
-  user: dbUrl.auth,
-  host: dbUrl.hostname,
-  port: dbUrl.port,
-});
-
-const dbName = dbUrl.path.slice(1).substring(0, dbUrl.path.slice(1).lastIndexOf('?'));
+const { auth: user, hostname: host, port, path } = url.parse(connectionString);
+const db = new Client({ user, host, port });
+const dbName = path.slice(1).substring(0, path.slice(1).lastIndexOf('?'));
 
 async function drop() {
   await db.connect();
