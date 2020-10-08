d4c314e64fab3a0cdfa9db50a16ab9fe2d62e546
Chris Ball <chris@echobind.com>
2020-10-07 21:30:39 -0400

    fix

--- cypress/integration/createBisonAppWithDefaults.test.js
+++ cypress/integration/createBisonAppWithDefaults.test.js
@@ -6,11 +6,28 @@ describe("Creating a new app", () => {
     cy.get("h2").contains(/home page/i);
   });
 
-  describe("prisma", () => {
-    it("sets DATABASE_URL", () => {
-      expect(process.env.DATABASE_URL).toBe(
+  describe("prisma .env", () => {
+    it("sets DATABASE_URL", async () => {
+      const appPath = process.env.APP_PATH;
+
+      cy.readFile(`${appPath}/prisma/.env`).should.contain(
         "postgresql://postgres@localhost:5432/foo_dev?schema=public"
       );
     });
   });
 });
+
+// const path = await getTmpDir();
+
+//   require("dotenv").config({
+//     path: `${path}/foo/prisma/.env`,
+//   });
+// });
+
+// describe("Prisma", () => {
+//   describe("Environment Variables", () => {
+//     it("correctly defines a DATABASE_URL env var", () => {
+//       expect(process.env.DATABASE_URL).toBe(
+//         "postgresql://postgres@localhost:5432/foo_dev?schema=public"
+//       );
+//     });
