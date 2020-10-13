8c070c85f5ab311f2db35299ddf04628bf221e05
Chris Ball <chris@echobind.com>
2020-10-13 19:21:23 -0400

    fix: ensure dotfiles are copied. Extract copyFiles util and add tests. (#83)
    * fix: use _.gitignore over .gitignore
    
    copying template/.gitignore did not work reliably.
    
    * dont need
    
    * fix: extract copyFiles task, add tests
    
    * wrap copyfiles

---
+++ tasks/copyFiles.js
@@ -0,0 +1,114 @@
+const path = require("path");
+const cpy = require("cpy");
+const fs = require("fs");
+
+const { copyWithTemplate } = require("../utils/copyWithTemplate");
+const {
+  copyDirectoryWithTemplate,
+} = require("../utils/copyDirectoryWithTemplate");
+
+const templateFolder = path.join(__dirname, "..", "template");
+const fromPath = (file) => path.join(templateFolder, file);
+
+/**
+ * Copies files based on variables and a target folder
+ */
+async function copyFiles({ variables, targetFolder }) {
+  const toPath = (file) => path.join(targetFolder, file);
+
+  // Files to render when using Heroku
+  const isHeroku = variables.host.name === "heroku";
+  let herokuFiles = [];
+
+  if (isHeroku) {
+    herokuFiles = [
+      copyWithTemplate(fromPath("app.json"), toPath("app.json"), variables),
+    ];
+  }
+
+  return Promise.all([
+    fs.promises.mkdir(targetFolder),
+
+    copyWithTemplate(
+      fromPath("package.json.ejs"),
+      toPath("package.json"),
+      variables
+    ),
+
+    copyWithTemplate(fromPath("README.md.ejs"), toPath("README.md"), variables),
+    copyWithTemplate(fromPath("_.gitignore"), toPath(".gitignore"), variables),
+
+    copyWithTemplate(
+      fromPath("_.env.local.ejs"),
+      toPath(".env.local"),
+      variables
+    ),
+
+    copyWithTemplate(
+      fromPath("_.env.test.ejs"),
+      toPath(".env.test"),
+      variables
+    ),
+
+    copyDirectoryWithTemplate(
+      fromPath(".github"),
+      toPath(".github"),
+      variables
+    ),
+
+    copyDirectoryWithTemplate(fromPath("pages"), toPath("pages"), variables),
+    copyDirectoryWithTemplate(fromPath("prisma"), toPath("prisma"), variables),
+
+    copyDirectoryWithTemplate(
+      fromPath("graphql"),
+      toPath("graphql"),
+      variables
+    ),
+
+    copyDirectoryWithTemplate(fromPath("tests"), toPath("tests"), variables),
+
+    ...herokuFiles,
+
+    cpy(
+      [
+        "_templates",
+        ".vscode",
+        "chakra",
+        "components",
+        "context",
+        "cypress",
+        "layouts",
+        "lib",
+        "prisma",
+        "!prisma/_.env*",
+        "public",
+        "scripts",
+        "services",
+        "utils",
+        ".eslintrc.js",
+        ".hygen.js",
+        ".tool-versions",
+        "api.graphql",
+        "codegen.yml",
+        "constants.ts",
+        "cypress.json",
+        "jest.config.js",
+        "next-env.d.ts",
+        "prettier.config.js",
+        "tsconfig.json",
+        "tsconfig.cjs.json",
+        "types.ts",
+      ],
+      targetFolder,
+      {
+        cwd: templateFolder,
+        // preserve path
+        parents: true,
+      }
+    ),
+  ]);
+}
+
+module.exports = {
+  copyFiles,
+};
