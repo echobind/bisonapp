030b838391d695c83ebadcc5f1ed7a235e21be2a
Chris Ball <chris@echobind.com>
2020-10-26 23:35:06 -0400

    fix logout test

--- template/tests/e2e/logout.test.js
+++ template/tests/e2e/logout.test.js
@@ -2,7 +2,7 @@ describe('Logout', () => {
   it('properly logs out a user', () => {
     const attrs = { password: 'superawesome' };
 
-    cy.createUserAndLogin({ email: user.email, password: attrs.password }).then((user) => {
+    cy.createUserAndLogin({ password: attrs.password }).then(() => {
       cy.visit('/');
       cy.findByText(/logout/i).click();
       cy.location('pathname').should('equal', '/login');
