31e214fb3fa9e2b1bb23a36f84c464630045d91d
Chris Ball <chris@echobind.com>
2021-05-11 10:03:01 -0400

    migrate user/profile relations to plain nexus

--- packages/create-bison-app/template/graphql/modules/profile.ts
+++ packages/create-bison-app/template/graphql/modules/profile.ts
@@ -5,15 +5,21 @@ export const Profile = objectType({
   name: 'Profile',
   description: 'A User Profile',
   definition(t) {
-    t.model.id();
-    t.model.firstName();
-    t.model.lastName();
-    t.model.createdAt();
-    t.model.updatedAt();
-    t.model.user();
+    t.nonNull.id('id');
+    t.nonNull.date('createdAt');
+    t.nonNull.date('updatedAt');
+    t.nonNull.string('firstName');
+    t.nonNull.string('lastName');
+    t.nonNull.field('user', {
+      type: 'User',
+      resolve: (parent, _, context) => {
+        return context.prisma.profile.findUnique({
+          where: { id: parent.id }
+        }).user()
+      }
+    })
 
     t.string('fullName', {
-      nullable: true,
       description: 'The first and last name of a user',
       resolve({ firstName, lastName }) {
         return [firstName, lastName].filter((n) => Boolean(n)).join(' ');
