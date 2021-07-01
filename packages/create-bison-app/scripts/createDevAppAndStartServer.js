98e9d642dd6871fcd4702268c6596a647e7f62a1
Kishan Gajera <kishan@echobind.com>
2021-07-01 10:20:06 -0400

    fix: symlinked files and directories not being removed

--- packages/create-bison-app/scripts/createDevAppAndStartServer.js
+++ packages/create-bison-app/scripts/createDevAppAndStartServer.js
@@ -19,8 +19,8 @@ async function init() {
   const { clean, port } = yargs(args).argv;
 
   if (clean) {
-    await fs.promises.rmdir(devAppPath, { recursive: true });
     await removeTemplateSymlinks();
+    await fs.promises.rm(devAppPath, { force: true, recursive: true });
   }
 
   // Create bison app if it does not exist
@@ -85,7 +85,8 @@ async function removeTemplateSymlinks() {
   const templatePath = relativePath => path.join(templateFolder, relativePath);
   const unlinkFile = async filename => {
     const filePath = templatePath(filename);
-    if (fs.existsSync(filePath) && fs.lstatSync(filePath).isSymbolicLink()) {
+    const lstat = fs.lstatSync(filePath, { throwIfNoEntry: false });
+    if (lstat && lstat.isSymbolicLink()) {
       await fs.promises.unlink(filePath);
     }
   }
@@ -94,8 +95,8 @@ async function removeTemplateSymlinks() {
   await unlinkFile("types.ts");
 
   // Directories cannot be "unlinked" so they must be removed
-  await fs.promises.rmdir(templatePath("node_modules"), { recursive: true });
-  await fs.promises.rmdir(templatePath("types"), { recursive: true });
+  await fs.promises.rm(templatePath("node_modules"), { force: true, recursive: true });
+  await fs.promises.rm(templatePath("types"), { force: true, recursive: true });
 }
 
 if (require.main === module) {
