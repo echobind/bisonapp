818e4be2b6322d27d32dcc3424f99aa81c6ee810
SamanthaShackelford <samantha@echobind.com>
2022-05-23 16:23:24 -0400

    fix: remove extra white space

--- packages/create-bison-app/template/__mocks__/fileMock.d.ts
+++ packages/create-bison-app/template/__mocks__/fileMock.d.ts
@@ -1,6 +1,5 @@
 declare module '*.svg' {
-    const content: any;
-    export const ReactComponent: any;
-    export default content;
+  const content: any;
+  export const ReactComponent: any;
+  export default content;
 }
-   
