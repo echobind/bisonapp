bcca0cba7aae8ae3a70fd69fac9c450baa927878
Cory Bosselmann <cbosselmann1029@gmail.com>
2022-06-10 13:26:25 -0400

    chore: cleanup unused functions

--- packages/create-bison-app/template/graphql/modules/scalars.ts
+++ packages/create-bison-app/template/graphql/modules/scalars.ts
@@ -6,33 +6,6 @@ import {
   URLResolver,
 } from 'graphql-scalars';
 import { asNexusMethod } from 'nexus';
-import { Kind, ObjectValueNode, ValueNode } from 'graphql';
-
-const parseObject = (ast: ObjectValueNode) => {
-  const value = Object.create(null);
-  ast.fields.forEach((field) => {
-    value[field.name.value] = parseAst(field.value);
-  });
-
-  return value;
-};
-
-const parseAst = (ast: ValueNode): any => {
-  switch (ast.kind) {
-    case Kind.STRING:
-    case Kind.BOOLEAN:
-      return ast.value;
-    case Kind.INT:
-    case Kind.FLOAT:
-      return parseFloat(ast.value);
-    case Kind.OBJECT:
-      return parseObject(ast);
-    case Kind.LIST:
-      return ast.values.map(parseAst);
-    default:
-      return null;
-  }
-};
 
 export const JSON = asNexusMethod(JSONObjectResolver, 'json');
 export const DateTime = asNexusMethod(DateTimeResolver, 'date');
