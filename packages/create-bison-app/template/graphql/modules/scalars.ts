6b890838eea332f04e7ba71a7b34547987bc3589
Cory Bosselmann <cbosselmann1029@gmail.com>
2022-06-10 13:12:40 -0400

    chore: refactor based on PR comments

--- packages/create-bison-app/template/graphql/modules/scalars.ts
+++ packages/create-bison-app/template/graphql/modules/scalars.ts
@@ -5,36 +5,8 @@ import {
   PhoneNumberResolver,
   URLResolver,
 } from 'graphql-scalars';
-import { asNexusMethod, scalarType } from 'nexus';
+import { asNexusMethod } from 'nexus';
 import { Kind, ObjectValueNode, ValueNode } from 'graphql';
-
-export const ComparisonOperatorScalarType = scalarType({
-  name: 'ComparisonOperator',
-  description: 'Allows values typically used in comparison operators: string, number, Date object',
-  // parseValue and serialize are used to ensure validity
-  parseValue: (value) => {
-    return typeof value === 'object' || typeof value === 'string' || typeof value === 'number'
-      ? value
-      : null;
-  },
-  serialize: (value) => {
-    return typeof value === 'object' || typeof value === 'string' || typeof value === 'number'
-      ? value
-      : null;
-  },
-  parseLiteral: (ast: ValueNode) => {
-    switch (ast.kind) {
-      case Kind.STRING:
-      case Kind.INT:
-        return ast.value;
-      case Kind.OBJECT:
-        return parseObject(ast);
-      default:
-        return null;
-    }
-  },
-  asNexusMethod: 'compare',
-});
 
 const parseObject = (ast: ObjectValueNode) => {
   const value = Object.create(null);
--- packages/create-bison-app/template/graphql/modules/shared.ts
+++ packages/create-bison-app/template/graphql/modules/shared.ts
@@ -14,12 +14,40 @@ export const StringFilter = inputObjectType({
     t.string('contains');
     t.string('endsWith');
     t.string('equals');
-    t.compare('gt');
-    t.compare('gte');
+    t.string('gt');
+    t.string('gte');
     t.list.nonNull.string('in');
-    t.compare('lt');
-    t.compare('lte');
+    t.string('lt');
+    t.string('lte');
     t.list.nonNull.string('notIn');
     t.string('startsWith');
+  },
+});
+
+export const NumberFilter = inputObjectType({
+  name: 'NumberFilter',
+  description: 'A way to filter number fields. Meant to pass to prisma where clause',
+  definition(t) {
+    t.int('equals');
+    t.int('gt');
+    t.int('gte');
+    t.list.nonNull.int('in');
+    t.int('lt');
+    t.int('lte');
+    t.list.nonNull.int('notIn');
+  },
+});
+
+export const DateFilter = inputObjectType({
+  name: 'DateFilter',
+  description: 'A way to filter date fields. Meant to pass to prisma where clause',
+  definition(t) {
+    t.date('equals');
+    t.date('gt');
+    t.date('gte');
+    t.list.nonNull.date('in');
+    t.date('lt');
+    t.date('lte');
+    t.list.nonNull.date('notIn');
   },
 });
