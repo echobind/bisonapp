bd8cde18fa657d21dd95fbff22920eef19b02555
Alex Anderson <aanderson@echobind.com>
2022-10-20 19:33:30 -0400

    Fix a few minor issues.

--- packages/create-bison-app/template/scripts/buildProd.ts
+++ packages/create-bison-app/template/scripts/buildProd.ts
@@ -1,7 +1,7 @@
 export {};
 const spawn = require('child_process').spawn;
 
-const DEFAULT_BUILD_COMMAND = `yarn build:types && yarn build:next`;
+const DEFAULT_BUILD_COMMAND = `yarn build:next`;
 
 /**
  * This builds the production app.
