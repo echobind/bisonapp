a8b58dd75f66634817f1711aa3ff7ee38b96b44c
Kishan Gajera <kishan@echobind.com>
2022-11-03 09:06:24 -0400

    refactor: remove JWT util functions and dependency

--- packages/create-bison-app/template/services/auth.ts
+++ packages/create-bison-app/template/services/auth.ts
@@ -3,8 +3,6 @@
  */
 
 import * as bcrypt from 'bcryptjs';
-import * as jwt from 'jsonwebtoken';
-import { User } from '@prisma/client';
 
 /**
  * Hashes a password using bcrypt
@@ -21,41 +19,4 @@ export function hashPassword(password: string): string {
  */
 export function comparePasswords(password: string, hashedPassword: string): boolean {
   return bcrypt.compareSync(password, hashedPassword);
-}
-
-const getAppSecret = (): string => {
-  const appSecret = process.env.APP_SECRET;
-
-  if (!appSecret) {
-    throw new Error('APP_SECRET is not set');
-  }
-
-  return appSecret;
-};
-
-/**
- * Signs a JWT for the provided user
- * @param user The user to return a JWT for
- */
-export const appJwtForUser = (user: Partial<User>): string => {
-  return jwt.sign({ userId: user.id }, getAppSecret());
-};
-
-/**
- * Parses and verifies a JWT token from header string
- * @param header The header to verify
- */
-export const verifyAuthHeader = (header?: string): JWT | undefined => {
-  if (!header) return;
-  const token = header.replace('Bearer ', '');
-
-  try {
-    return jwt.verify(token, getAppSecret()) as JWT;
-  } catch (e) {
-    return;
-  }
-};
-
-interface JWT {
-  userId: string;
 }
