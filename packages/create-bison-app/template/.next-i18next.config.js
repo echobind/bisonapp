50054b64c51736c1543fbfefd14e6fcd383dabba
Matt Thompson <matt@echobind.com>
2022-12-10 21:25:34 -0500

    feat: Add i18next and i18next-http-backend for new service util

---
+++ packages/create-bison-app/template/.next-i18next.config.js
@@ -0,0 +1,8 @@
+// https://nextjs.org/docs/advanced-features/i18n-routing
+
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
+  },
+};
--- packages/create-bison-app/template/pages/index.tsx
+++ packages/create-bison-app/template/pages/index.tsx
@@ -2,12 +2,15 @@ import Head from 'next/head';
 import { Heading, Center } from '@chakra-ui/react';
 import { useSession } from 'next-auth/react';
 
+import { translator } from '@/utils/i18n-translator';
+
 function Home() {
   const { data: session } = useSession();
 
-  const name = session?.user?.profile?.firstName;
+  const firstName = session?.user?.profile?.firstName;
 
-  const welcomeMsg = name ? `Welcome, ${name}!` : 'Welcome, Guest!';
+  const t = translator({ ns: ['common'] });
+  const welcomeMsg = t('welcome', { firstName });
 
   return (
     <>
