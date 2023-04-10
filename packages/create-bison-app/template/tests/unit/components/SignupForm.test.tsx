7d7c1b34e3d7921239c2a6ea92c7f18e1fadf95d
Cully Larson <cully.larson@gmail.com>
2023-04-10 09:39:11 -0700

    chore: Update package dependencies. (#309)
    * chore: Update create-bison-app packages.
    
    * chore: Update all Bison packages.
    
    * fix: Next 13 links don't require an anchor child anymore.
    
    * fix: Add forward mock for NextRouter in jest tests.
    
    * fix: Make react-testing-library tests pass.

--- packages/create-bison-app/template/tests/unit/components/SignupForm.test.tsx
+++ packages/create-bison-app/template/tests/unit/components/SignupForm.test.tsx
@@ -5,32 +5,25 @@
 import { render, waitFor, userEvent, fireEvent, act } from '@/tests/utils';
 import { SignUpForm } from '@/components/auth/SignUpForm';
 
+// need this in order to use `act`
+((global || {}) as any).IS_REACT_ACT_ENVIRONMENT = true;
+
 describe('SignUpForm', () => {
   it('loads', async () => {
     expect(() => render(<SignUpForm />)).not.toThrowError();
   });
 
   describe('Validation', () => {
-    it('shows errors when email invalid', async () => {
-      const { getByLabelText, getByText } = render(<SignUpForm />);
-
-      // update email
-      const emailInput = getByLabelText(/email/i);
-      userEvent.type(emailInput, 'notemail');
-
-      // submit form
-      const submitButton = getByText(/sign up/i);
-      userEvent.click(submitButton);
-
-      await waitFor(() => expect(emailInput).toHaveAttribute('aria-invalid', 'true'));
-
-      userEvent.type(emailInput, 'asdf@wee.net');
-
-      await waitFor(() => expect(emailInput).not.toHaveAttribute('aria-invalid', 'true'));
-    });
-
     it('shows errors when password invalid', async () => {
-      const { getAllByLabelText, getByText } = render(<SignUpForm />);
+      const { getByLabelText, getAllByLabelText, getByText } = render(<SignUpForm />);
+
+      // need to provide a value for these fields or else the form will not
+      // submit
+      const emailInput = getByLabelText(/email/i);
+      userEvent.type(emailInput, 'test@example.com');
+
+      const confirmPasswordInput = getByLabelText(/confirm password/i);
+      userEvent.type(confirmPasswordInput, 'ihave8characters');
 
       // update password
       const passwordInput = getAllByLabelText(/password/i)[0];
@@ -40,12 +33,12 @@ describe('SignUpForm', () => {
       const submitButton = getByText(/sign up/i);
       userEvent.click(submitButton);
 
-      await waitFor(() => expect(passwordInput).toHaveAttribute('aria-invalid', 'true'));
+      await waitFor(() => expect(passwordInput).toBeInvalid());
 
       userEvent.clear(passwordInput);
       userEvent.type(passwordInput, 'ihave8characters');
 
-      await waitFor(() => expect(passwordInput).not.toHaveAttribute('aria-invalid', 'true'));
+      await waitFor(() => expect(passwordInput).toBeValid());
     });
   });
 
@@ -61,7 +54,7 @@ describe('SignUpForm', () => {
       const confirmPasswordInput = getByLabelText(/confirm password/i);
       const submitButton = getByText(/sign up/i);
 
-      await act(() => {
+      act(() => {
         fireEvent.change(firstNameInput, { target: { value: 'Barry' } });
         fireEvent.change(lastNameInput, { target: { value: 'Allen' } });
         fireEvent.change(emailInput, { target: { value: 'ballen@speedforce.net' } });
@@ -70,11 +63,11 @@ describe('SignUpForm', () => {
         fireEvent.submit(submitButton);
       });
 
-      await waitFor(() => expect(firstNameInput).not.toHaveAttribute('aria-invalid', 'true'));
-      await waitFor(() => expect(lastNameInput).not.toHaveAttribute('aria-invalid', 'true'));
-      await waitFor(() => expect(emailInput).not.toHaveAttribute('aria-invalid', 'true'));
-      await waitFor(() => expect(passwordInput).not.toHaveAttribute('aria-invalid', 'true'));
-      await waitFor(() => expect(confirmPasswordInput).not.toHaveAttribute('aria-invalid', 'true'));
+      await waitFor(() => expect(firstNameInput).toBeValid());
+      await waitFor(() => expect(lastNameInput).toBeValid());
+      await waitFor(() => expect(emailInput).toBeValid());
+      await waitFor(() => expect(passwordInput).toBeValid());
+      await waitFor(() => expect(confirmPasswordInput).toBeValid());
 
       // Test Toast Message
       // Issue w/ React Hook Form and Next Auth -- asserted validation passed
