125da410deeb796d9e21cd13b099c45dd018e7b5
Alex Anderson <aanderson@echobind.com>
2023-04-21 09:24:04 -0400

    Fix a build issue.

--- packages/create-bison-app/template/tests/utils.tsx
+++ packages/create-bison-app/template/tests/utils.tsx
@@ -99,7 +99,6 @@ const mockRouter: NextRouter = {
   replace: jest.fn(),
   reload: jest.fn(),
   back: jest.fn(),
-  forward: jest.fn(),
   prefetch: jest.fn(() => Promise.resolve()),
   beforePopState: jest.fn(),
   events: {
